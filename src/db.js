adb9fdb80968f098bbae0d0cce4bf22034fd913c
nelson <nelson@gmail.com>
2026-05-17 23:08:01 -0500

    conectar supabase

--- src/db.js
+++ src/db.js
@@ -7,13 +7,14 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   user:     process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  ssl:      { rejectUnauthorized: false },
 });
 
 pool.connect((err, client, release) => {
   if (err) {
-    console.error(' Error conectando a la BD:', err.message);
+    console.error('❌ Error conectando a la BD:', err.message);
   } else {
-    console.log(' Conectado a Empleos_database');
+    console.log('✅ Conectado a Supabase');
     release();
   }
 });
